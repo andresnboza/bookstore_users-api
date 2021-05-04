bd29419912a05ac53199ff8be00e52e8901412a9
andres.navarrete <andres.navarrete@ibm.com>
2021-05-04 14:24:36 -0600

    update

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,15 +1,10 @@
 package app
 
 import (
-	"github.com/andresnboza/bookstore_users-api/controllers/ping/ping"
 	"github.com/andresnboza/bookstore_users-api/controllers/users"
 )
 
 func mapUrls() {
-	router.GET("/", ping.ServerHello)
-
-	router.GET("/ping", ping.Ping)
-
 	router.GET("/users/:user_id", users.GetUser)
 	router.POST("/users/", users.CreateUser)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/andresnboza/bookstore_users-api
+module bookstore_users-api
 
-go 1.16
+go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.7.1
-	github.com/go-sql-driver/mysql v1.6.0
+	github.com/andresnboza/bookstore_users-api v0.0.0-20210504184306-7798f19008e6 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 )
